1cf01995c3cfd23910474eecd7ec6fc7fdc882cf
Loh Siu Yin <siuyin@beyondbroadcast.com>
2026-03-13 11:02:15 +0800

    feat: add sse streaming example

--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module example
 go 1.25.6
 
 require github.com/siuyin/dflt v0.0.0-20230329062002-0475f4d54412
+
+require (
+	github.com/CAFxX/httpcompression v0.0.9 // indirect
+	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/starfederation/datastar-go v1.1.0
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/siuyin/dflt"
+	"github.com/starfederation/datastar-go/datastar"
 )
 
 var t *template.Template
@@ -14,6 +16,16 @@ var t *template.Template
 func main() {
 	port := dflt.EnvString("PORT", "8080")
 	log.Printf("PORT=%s", port)
+	http.HandleFunc("/time", timeHandler)
 	http.Handle("/", http.FileServer(http.FS(public.Content)))
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	sse := datastar.NewSSE(w, r)
+	for {
+		tm := time.Now().Format("2006-01-02 15:04:05.000 -07:00")
+		sse.PatchElementf(`<div id="time" style="margin-top: var(--size-3);">%s</div>`, tm)
+		time.Sleep(100 * time.Millisecond)
+	}
 }
